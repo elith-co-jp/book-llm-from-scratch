3981f933a68b36705e7b19b477605eccfe70373d
aRySt0cat <ryuta.academic@gmail.com>
2024-02-12 16:27:48 +0900

    Add transformer

--- llm_from_scratch/transformer/transformer.py
+++ llm_from_scratch/transformer/transformer.py
@@ -1,7 +1,7 @@
-from torch import nn
-from torch import Tensor
+from torch import Tensor, nn
+
 from llm_from_scratch.transformer.attention import MultiHeadAttention
-from llm_from_scratch.transformer.utils import LayerNorm, sinusoidal_position_encoding
+from llm_from_scratch.transformer.utils import LayerNorm, PositionalEncoding
 
 
 class EncoderBlock(nn.Module):
@@ -17,29 +17,54 @@ class EncoderBlock(nn.Module):
         self.layer_norm2 = LayerNorm(d_model)
 
     def forward(self, x: Tensor, mask: Tensor) -> Tensor:
-        x_attention = self.attention(x, x, x)
+        x_attention = self.attention(x, x, x, mask=mask)
         x = self.layer_norm1(x + x_attention)
         x_ff = self.feed_forward(x)
         x = self.layer_norm2(x + x_ff)
         return x
 
-class Encoder(nn.Module):
-    def __init__(self, d_model: int, n_blocks: int, n_heads: int, d_k: int, d_v: int, d_ff: int):
-        super().__init__()
-        self.blocks = nn.ModuleList([EncoderBlock(d_model, n_heads, d_k, d_v, d_ff) for _ in range(n_blocks)])
 
-    def forward(self, x: Tensor) -> Tensor:
+class Encoder(nn.Module):
+    def __init__(
+        self,
+        vocabulary_size: int,
+        max_sequence_len: int,
+        d_model: int,
+        n_blocks: int,
+        n_heads: int,
+        d_k: int,
+        d_v: int,
+        d_ff: int,
+    ):
+        super().__init__()
+        self.embedding = nn.Embedding(vocabulary_size, d_model)
+        self.pe = PositionalEncoding(d_model, max_sequence_len)
+        self.blocks = nn.ModuleList(
+            [EncoderBlock(d_model, n_heads, d_k, d_v, d_ff) for _ in range(n_blocks)]
+        )
+
+    def forward(self, x: Tensor, mask: Tensor | None = None) -> Tensor:
+        """Forward.
+
+        Args:
+            x (Tensor): Input tensor. shapeは(batch_size, sequence_length, 1).
+
+        Returns:
+            Tensor: Output tensor. shapeは(batch_size, sequence_length, d_model).
+        """
+        x = self.embedding(x)
+        x = self.pe(x)
         for block in self.blocks:
-            x = block(x)
+            x = block(x, mask=mask)
         return x
 
 
 class DecoderBlock(nn.Module):
-    def __init__(self, d_model: int, n_blocks: int, n_heads: int, d_k: int, d_v: int, d_ff: int):
+    def __init__(self, d_model: int, n_heads: int, d_k: int, d_v: int, d_ff: int):
         super().__init__()
         self.attention = MultiHeadAttention(n_heads, d_k, d_v, d_model)
         self.layer_norm1 = LayerNorm(d_model)
-        self.attention_souce_target = MultiHeadAttention(n_heads, d_k, d_v, d_model)
+        self.attention_source_target = MultiHeadAttention(n_heads, d_k, d_v, d_model)
         self.layer_norm2 = LayerNorm(d_model)
         self.feed_forward = nn.Sequential(
             nn.Linear(d_model, d_ff),
@@ -48,21 +73,94 @@ class DecoderBlock(nn.Module):
         )
         self.layer_norm3 = LayerNorm(d_model)
 
-    def forward(self, x: Tensor, encoder_output: Tensor, mask: Tensor|None = None, src_tgt_mask: Tensor|None = None) -> Tensor:
+    def forward(
+        self,
+        x: Tensor,
+        encoder_output: Tensor,
+        mask: Tensor | None = None,
+        src_tgt_mask: Tensor | None = None,
+    ) -> Tensor:
         x_attention = self.attention(x, x, x, mask=mask)
         x = self.layer_norm1(x + x_attention)
-        x_attention_souce_target = self.attention_souce_target(x, encoder_output, encoder_output, mask=src_tgt_mask)
-        x = self.layer_norm2(x + x_attention_souce_target)
+        x_attention_source_target = self.attention_source_target(
+            x, encoder_output, encoder_output, mask=src_tgt_mask
+        )
+        x = self.layer_norm2(x + x_attention_source_target)
         x_ff = self.feed_forward(x)
         x = self.layer_norm3(x + x_ff)
         return x
 
-class Decoder(nn.Module):
-    def __init__(self, d_model: int, n_blocks: int, n_heads: int, d_k: int, d_v: int, d_ff: int):
-        super().__init__()
-        self.blocks = nn.ModuleList([DecoderBlock(d_model, n_heads, d_k, d_v, d_ff) for _ in range(n_blocks)])
 
-    def forward(self, x: Tensor, encoder_output: Tensor, mask: Tensor|None = Tensor) -> Tensor:
+class Decoder(nn.Module):
+    def __init__(
+        self,
+        vocabulary_size: int,
+        max_sequence_len: int,
+        d_model: int,
+        n_blocks: int,
+        n_heads: int,
+        d_k: int,
+        d_v: int,
+        d_ff: int,
+    ):
+        super().__init__()
+        self.embedding = nn.Embedding(vocabulary_size, d_model)
+        self.pe = PositionalEncoding(d_model, max_sequence_len)
+        self.blocks = nn.ModuleList(
+            [DecoderBlock(d_model, n_heads, d_k, d_v, d_ff) for _ in range(n_blocks)]
+        )
+
+    def forward(
+        self,
+        x: Tensor,
+        encoder_output: Tensor,
+        mask: Tensor | None = None,
+        src_tgt_mask: Tensor | None = None,
+    ) -> Tensor:
+        x = self.embedding(x)
+        x = self.pe(x)
         for block in self.blocks:
-            x = block(x, encoder_output, mask=mask)
+            x = block(x, encoder_output, mask=mask, src_tgt_mask=src_tgt_mask)
         return x
+
+
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        src_vocab_size: int,
+        tgt_vocab_size: int,
+        max_sequence_len: int,
+        d_model: int,
+        n_blocks: int,
+        n_heads: int,
+        d_k: int,
+        d_v: int,
+        d_ff: int,
+    ):
+        super().__init__()
+        self.encoder = Encoder(
+            src_vocab_size, max_sequence_len, d_model, n_blocks, n_heads, d_k, d_v, d_ff
+        )
+        self.decoder = Decoder(
+            tgt_vocab_size, max_sequence_len, d_model, n_blocks, n_heads, d_k, d_v, d_ff
+        )
+        self.linear = nn.Linear(d_model, tgt_vocab_size, bias=False)
+
+    def forward(
+        self,
+        src: Tensor,
+        tgt: Tensor,
+        src_mask: Tensor | None = None,
+        tgt_mask: Tensor | None = None,
+        src_tgt_mask: Tensor | None = None,
+    ) -> Tensor:
+        encoder_output = self.encoder(src, mask=src_mask)
+        decoder_output = self.decoder(
+            tgt, encoder_output, mask=tgt_mask, src_tgt_mask=src_tgt_mask
+        )
+        output = self.linear(decoder_output)
+        return output
+
+    def inference(self):
+        # Auto-Regressive に推論する
+        pass
